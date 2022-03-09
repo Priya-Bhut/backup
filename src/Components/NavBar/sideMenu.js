22dc0d3b701043abd72fbea66fdba197867e4e2f
Heet-Sheth <heetsheth0000@gmail.com>
2022-03-09 18:05:47 +0530

    Hover and DropDown Fixed

--- src/Components/NavBar/sideMenu.js
+++ src/Components/NavBar/sideMenu.js
@@ -10,6 +10,7 @@ function SideMenu(props) {
   const { setterFunction, dataSetter, data } = props;
   const [menuExpand, setMenuExpand] = useState(false);
   const [expandedData, setExpandedData] = useState([]);
+  const [submenu, setSubmenu] = useState(0);
   useEffect(() => {
     setExpandedData(data);
   }, [data]);
@@ -22,12 +23,18 @@ function SideMenu(props) {
 
       <div className="container">
         <div className={`data-left${menuExpand ? "-opened" : ""}`}>
-          <span className="side-menu-item">
-            <NavLink to="/home" className="link">
+          <NavLink to="/home" className="side-menu-item">
+            <span
+              className="link internal-item-left"
+              onClick={() => {
+                if (submenu !== 0 && submenu === 1) setSubmenu(0);
+                else setSubmenu(1);
+              }}
+            >
               <i className="fa fa-home" />
-            </NavLink>
+            </span>
             {menuExpand ? null : (
-              <div className="side-box-container one">
+              <div className="side-box-container">
                 <span className="side-box-header">Home</span>
                 <span className="side-box-item">Alignments</span>
                 <span className="side-box-item">Action Center</span>
@@ -35,13 +42,44 @@ function SideMenu(props) {
                 <span className="side-box-item">Notes</span>
               </div>
             )}
-          </span>
-          <span className="side-menu-item">
-            <NavLink to="/okrs" className="link">
+            {menuExpand ? (
+              <span className="internal-item-right">
+                <span
+                  className="bold-font"
+                  onClick={() => {
+                    if (submenu !== 0 && submenu === 1) setSubmenu(0);
+                    else setSubmenu(1);
+                  }}
+                >
+                  Home &nbsp;
+                  <i
+                    className={`fa fa-chevron-${
+                      submenu === 1 ? "up" : "down"
+                    } arrow`}
+                  />
+                </span>
+                {submenu === 1 ? (
+                  <div className="internal-sub-menu">
+                    {expandedData.map((items) => (
+                      <span className="internal-sub-item">{items}</span>
+                    ))}
+                  </div>
+                ) : null}
+              </span>
+            ) : null}
+          </NavLink>
+          <NavLink to="/okrs" className="side-menu-item">
+            <span
+              className="link internal-item-left"
+              onClick={() => {
+                if (submenu !== 0 && submenu === 2) setSubmenu(0);
+                else setSubmenu(2);
+              }}
+            >
               <i className="fa fa-crosshairs" />
-            </NavLink>
+            </span>
             {menuExpand ? null : (
-              <div className="side-box-container two">
+              <div className="side-box-container">
                 <span className="side-box-header">OKRs</span>
                 <span className="side-box-item">All My OKRs</span>
                 <span className="side-box-item">My Individual OKRs</span>
@@ -49,13 +87,44 @@ function SideMenu(props) {
                 <span className="side-box-item">My Org's OKRs</span>
               </div>
             )}
-          </span>
-          <span className="side-menu-item">
-            <NavLink to="/tasks" className="link">
+            {menuExpand ? (
+              <span className="internal-item-right">
+                <span
+                  className="bold-font"
+                  onClick={() => {
+                    if (submenu !== 0 && submenu === 2) setSubmenu(0);
+                    else setSubmenu(2);
+                  }}
+                >
+                  OKRs &nbsp;
+                  <i
+                    className={`fa fa-chevron-${
+                      submenu === 2 ? "up" : "down"
+                    } arrow`}
+                  />
+                </span>
+                {submenu === 2 ? (
+                  <div className="internal-sub-menu">
+                    {expandedData.map((items) => (
+                      <span className="internal-sub-item">{items}</span>
+                    ))}
+                  </div>
+                ) : null}
+              </span>
+            ) : null}
+          </NavLink>
+          <NavLink to="/tasks" className="side-menu-item">
+            <span
+              className="link internal-item-left"
+              onClick={() => {
+                if (submenu !== 0 && submenu === 3) setSubmenu(0);
+                else setSubmenu(3);
+              }}
+            >
               <i className="fa fa-tasks" />
-            </NavLink>
+            </span>
             {menuExpand ? null : (
-              <div className="side-box-container three">
+              <div className="side-box-container">
                 <span className="side-box-header">Tasks</span>
                 <span className="side-box-item">My Space</span>
                 <span className="side-box-item">My Tasks</span>
@@ -63,13 +132,44 @@ function SideMenu(props) {
                 <span className="side-box-item">My Org's Tasks</span>
               </div>
             )}
-          </span>
-          <span className="side-menu-item">
-            <NavLink to="/cfrs" className="link">
+            {menuExpand ? (
+              <span className="internal-item-right">
+                <span
+                  className="bold-font"
+                  onClick={() => {
+                    if (submenu !== 0 && submenu === 3) setSubmenu(0);
+                    else setSubmenu(3);
+                  }}
+                >
+                  Tasks&nbsp;
+                  <i
+                    className={`fa fa-chevron-${
+                      submenu === 3 ? "up" : "down"
+                    } arrow`}
+                  />
+                </span>
+                {submenu === 3 ? (
+                  <div className="internal-sub-menu">
+                    {expandedData.map((items) => (
+                      <span className="internal-sub-item">{items}</span>
+                    ))}
+                  </div>
+                ) : null}
+              </span>
+            ) : null}
+          </NavLink>
+          <NavLink to="/cfrs" className="side-menu-item">
+            <span
+              className="link internal-item-left"
+              onClick={() => {
+                if (submenu !== 0 && submenu === 4) setSubmenu(0);
+                else setSubmenu(4);
+              }}
+            >
               <i className="fa fa-calendar" />
-            </NavLink>
+            </span>
             {menuExpand ? null : (
-              <div className="side-box-container four">
+              <div className="side-box-container">
                 <span className="side-box-header">CFRs</span>
                 <span className="side-box-item">Leader Board</span>
                 <span className="side-box-item">My Awards</span>
@@ -77,27 +177,45 @@ function SideMenu(props) {
                 <span className="side-box-item">Given</span>
               </div>
             )}
-          </span>
+            {menuExpand ? (
+              <span className="internal-item-right">
+                <span
+                  className="bold-font"
+                  onClick={() => {
+                    if (submenu !== 0 && submenu === 4) setSubmenu(0);
+                    else setSubmenu(4);
+                  }}
+                >
+                  CFRs&nbsp;
+                  <i
+                    className={`fa fa-chevron-${
+                      submenu === 4 ? "up" : "down"
+                    } arrow`}
+                  />
+                </span>
+                {submenu === 4 ? (
+                  <div className="internal-sub-menu">
+                    {expandedData.map((items) => (
+                      <span className="internal-sub-item">{items}</span>
+                    ))}
+                  </div>
+                ) : null}
+              </span>
+            ) : null}
+          </NavLink>
+          <Route exact path="/Home">
+            <HOME dataSetter={dataSetter} />
+          </Route>
+          <Route exact path="/okrs">
+            <OKR dataSetter={dataSetter} />
+          </Route>
+          <Route exact path="/tasks">
+            <TASK dataSetter={dataSetter} />
+          </Route>
+          <Route exact path="/cfrs">
+            <CFR dataSetter={dataSetter} />
+          </Route>
         </div>
-        {menuExpand ? (
-          <div className="data-left">
-            <Route exact path="/home">
-              <HOME dataSetter={dataSetter} />
-            </Route>
-            <Route exact path="/okrs">
-              <OKR dataSetter={dataSetter} />
-            </Route>
-            <Route exact path="/tasks">
-              <TASK dataSetter={dataSetter} />
-            </Route>
-            <Route exact path="/cfrs">
-              <CFR dataSetter={dataSetter} />
-            </Route>
-            {expandedData.map((item) => (
-              <span className="side-menu-item">{item}</span>
-            ))}
-          </div>
-        ) : null}
       </div>
     </div>
   );
