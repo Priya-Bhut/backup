87a99cb58270cb1527f7ee7b31a8de19c7f0ae5c
Heet-Sheth <heetsheth0000@gmail.com>
2022-03-10 12:14:15 +0530

    Top Below Bar Done

--- src/Components/RightBody/topNav.js
+++ src/Components/RightBody/topNav.js
@@ -6,15 +6,34 @@ function TopNav(props) {
   const [expandProfile, setExpandProfile] = useState(false);
   return (
     <div className={`top-nav ${doShrink ? "shrinked" : null}`}>
-      <span className="logo-text">BrilworksOKR</span>
-      <div
-        className="profile-container"
-        onClick={() => setExpandProfile(!expandProfile)}
-      >
-        <span className="profile-name">Brilworks</span>
-        <span className="profile-expand">{expandProfile ? "▴" : "▾"}</span>
+      <div className="inner-top">
+        <span className="logo-text">BrilworksOKR</span>
+        <div
+          className="profile-container"
+          onClick={() => setExpandProfile(!expandProfile)}
+        >
+          <span className="profile-name">Brilworks</span>
+          <span className="profile-expand">{expandProfile ? "▴" : "▾"}</span>
+        </div>
+        {expandProfile ? <ProfileDropDown /> : null}
       </div>
-      {expandProfile ? <ProfileDropDown /> : null}
+      <hr width="100%" />
+      <div className="inner-bottom">
+        <span className="logo-text">Projects</span>
+        <div className="inner-bottom-right">
+          <div className="search-container">
+            <input
+              type="text"
+              className="filter-search"
+              placeholder="Search..."
+            />
+            <i className="fa fa-search" />
+          </div>
+          <button className="project-button">
+            <i className="fa fa-plus-square" /> &nbsp; Add Project
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
