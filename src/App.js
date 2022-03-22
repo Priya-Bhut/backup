e035ab14f763044068f8d62b33780c6ca3a263a0
Arshit_Koshiya <arshit.n.p.8@gmail.com>
2022-03-22 12:17:49 +0530

    SecondHeader Changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
-import Graph from './Components/Graph/Graph';
+// import Graph from './Components/Graph/Graph';
 import IndividualOKR from './Components/OKR/IndividualOKR';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useCallback, useState } from 'react';
@@ -50,32 +50,34 @@ function App() {
 
       <Routes>
         <Route exact restricted={true} path='/:organisationUrl/*' element={<Layout />}>
-          <Route exact path='alignments' element={<Graph />} />
-          <Route path='action-center' exact element={<div>Coming Soon...</div>} />
-          <Route path='dependencies' exact element={<div>Coming Soon...</div>} />
-          <Route path='notes' exact element={<div>Coming Soon...</div>} />
-          <Route path='company-dashboard' exact element={<div>Coming Soon...</div>} />
-          <Route path='dept-hierarchy' exact element={<div>Coming Soon...</div>} />
-          <Route path='news-feed' exact element={<div>Coming Soon...</div>} />
-          <Route path='org-hierarchy' exact element={<div>Coming Soon...</div>} />
-          <Route exact path='all' element={<IndividualOKR handleAlert={handleAlert} />} />
-          <Route path='individual' exact element={<div>Coming Soon...</div>} />
-          <Route path='developer' exact element={<div>Coming Soon...</div>} />
-          <Route path='org' exact element={<div>Coming Soon...</div>} />
-          <Route path='watched' exact element={<div>Coming Soon...</div>} />
-          <Route path='shared' exact element={<div>Coming Soon...</div>} />
-          <Route path='emplyee' exact element={<div>Coming Soon...</div>} />
-          <Route path='department' exact element={<div>Coming Soon...</div>} />
-          <Route path='manager' exact element={<div>Coming Soon...</div>} />
-          <Route path='*' exact element={<div></div>} />
-          <Route path='my-task' exact element={<div>Coming Soon...</div>} />
-          <Route path='task-others' exact element={<div>Coming Soon...</div>} />
-          <Route path='org-task' exact element={<div>Coming Soon...</div>} />
-          <Route path='empl-task' exact element={<div>Coming Soon...</div>} />
-          <Route path='received' exact element={<div>Coming Soon...</div>} />
-          <Route path='given' exact element={<div>Coming Soon...</div>} />
-          <Route path='award-received' exact element={<div>Coming Soon...</div>} />
-          <Route path='award-given' exact element={<div>Coming Soon...</div>} />
+          <Route exact path='alignments' element={<div>Comming Soon...</div>} />
+          <Route path='action-center' exact element={<div>Comming Soon...</div>} />
+          <Route path='dependencies' exact element={<div>Comming Soon...</div>} />
+          <Route path='notes' exact element={<div>Comming Soon...</div>} />
+          <Route path='company-dashboard' exact element={<div>Comming Soon...</div>} />
+          <Route path='dept-hierarchy' exact element={<div>Comming Soon...</div>} />
+          <Route path='news-feed' exact element={<div>Comming Soon...</div>} />
+          <Route path='org-hierarchy' exact element={<div>Comming Soon...</div>} />
+          <Route path='all' element={<IndividualOKR handleAlert={handleAlert} />} />
+          <Route path='IndividualOKRs' element={<IndividualOKR handleAlert={handleAlert} />} />
+          <Route path='CorporateOKRs' element={<div>Comming Soon...</div>} />
+          <Route path='individual' exact element={<div>Comming Soon...</div>} />
+          <Route path='developer' exact element={<div>Comming Soon...</div>} />
+          <Route path='org' exact element={<div>Comming Soon...</div>} />
+          <Route path='watched' exact element={<div>Comming Soon...</div>} />
+          <Route path='shared' exact element={<div>Comming Soon...</div>} />
+          <Route path='emplyee' exact element={<div>Comming Soon...</div>} />
+          <Route path='department' exact element={<div>Comming Soon...</div>} />
+          <Route path='manager' exact element={<div>Comming Soon...</div>} />
+          <Route path='*' exact element={<div>Comming Soon...</div>} />
+          <Route path='my-task' exact element={<div>Comming Soon...</div>} />
+          <Route path='task-others' exact element={<div>Comming Soon...</div>} />
+          <Route path='org-task' exact element={<div>Comming Soon...</div>} />
+          <Route path='empl-task' exact element={<div>Comming Soon...</div>} />
+          <Route path='received' exact element={<div>Comming Soon...</div>} />
+          <Route path='given' exact element={<div>Comming Soon...</div>} />
+          <Route path='award-received' exact element={<div>Comming Soon...</div>} />
+          <Route path='award-given' exact element={<div>Comming Soon...</div>} />
         </Route>
         <Route path='*' element={<Home />} />
       </Routes>
--- src/Components/OKR/SecondHeader.js
+++ src/Components/OKR/SecondHeader.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
 
 function SecondHeader(props) {
+  const { organisationUrl } = useParams() || {};
   return (
     <div className='secondHeader'>
       <h5>OKR</h5>
       <div className='slideTab'>
-        <div className='IndividualOKRs'>Individual-OKRs</div>
-        <div className='CorporateOKRs'>Corporate-OKRs</div>
+        <Link to={`/${organisationUrl}/IndividualOKRs`}>
+          <span className='OKRs'>Individual-OKRs</span>
+        </Link>
+        <Link to={`/${organisationUrl}/CorporateOKRs`}>
+          <span className='OKRs'>Corporate-OKRs</span>
+        </Link>
       </div>
 
       <Button className='addDesignationButton brilCrmButton' onClick={props?.addNewOkr}>
