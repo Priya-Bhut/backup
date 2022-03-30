3cb7f21859da4aca8ed1cc7068018778cb7b8e13
brilworks_sachin <sachinbabu.y@brilworks.com>
2022-03-30 18:48:39 +0530

    Env added

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -1,6 +1,7 @@
+/*eslint-disable*/
 const imgUrl = '';
-const apiUrl = 'http://brilworks.hopto.org:3001/rest/';
-const okrUrl = 'http://brilworks.hopto.org:3105/okr/rest/';
-const APP_ENV = 'devlopment';
+const apiUrl = process.env.REACT_APP_apiUrl;
+const okrUrl = process.env.REACT_APP_okrUrl;
+const APP_ENV = process.env.REACT_APP_APP_ENV;
 
 export { imgUrl, apiUrl, okrUrl, APP_ENV };
